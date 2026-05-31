dadbdd31af6d4000b6bbcc2a7865b9ea6a4399d9
Piyush-Kr-19 <piyush14359@gmail.com>
2026-05-31 21:31:51 +0530

    Problem 3 Done

--- lists_practice.py
+++ lists_practice.py
@@ -13,3 +13,37 @@ for i,j in zip(fruits,vegetables):
     result.append([i,j])
 
 print(result)
+
+# Problem 2: Add new item to list after a specified item
+
+# Write a program to add item 7000 after 6000 in the following Python List
+
+
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+
+print(list1[2][2].append(7000))
+
+print(list1)
+
+# Problem 3: Update no of items available
+# Suppose you are given a list of candy and another list of same size representing no of items of respective candy.
+
+# i.e -
+
+# candy_list = ['Jelly Belly','Kit Kat','Double Bubble','Milky Way','Three Musketeers']
+# no_of_items = [10,20,34,74,32]
+# Write a program to show no. of items of each candy type.
+
+# Output:
+
+# Jelly Belly-10
+# Kit Kat-20
+# Double Bubble-34
+# Milky Way-74
+# Three Musketeers-32
+
+candy_list = ['Jelly Belly','Kit Kat','Double Bubble','Milky Way','Three Musketeers']
+no_of_items = [10,20,34,74,32]
+
+for i , j in zip(candy_list,no_of_items):
+    print(f"{i}-{j}")
