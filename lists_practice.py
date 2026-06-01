ff739872014d65a2eb88cc874f90b442ca31934e
Piyush-Kr-19 <piyush14359@gmail.com>
2026-06-01 17:45:32 +0530

    Problem 4 Done

--- lists_practice.py
+++ lists_practice.py
@@ -47,3 +47,26 @@ no_of_items = [10,20,34,74,32]
 
 for i , j in zip(candy_list,no_of_items):
     print(f"{i}-{j}")
+    
+    
+# Problem 4: Running Sum on list
+# Write a program to print a list after performing running sum on it.
+
+# i.e:
+
+# Input:
+
+# list1 = [1,2,3,4,5,6]
+# Output:
+
+# [1,3,6,10,15,21]
+
+list1 = [1,2,3,4,5,6]
+
+sum = 0
+result = []
+
+for i in list1:
+    sum += i
+    result.append(sum)
+print(result)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+list1 = [1,2,3,4,5,6]
+
+sum = 0
+result = []
+
+for i in list1:
+    sum += i
+    result.append(sum)
+print(result)
