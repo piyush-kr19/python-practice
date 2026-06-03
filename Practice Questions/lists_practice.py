526a77e9721256a8149774ad69dbe2821eabf0c2
Piyush-Kr-19 <piyush14359@gmail.com>
2026-06-03 16:31:14 +0530

    Created two different folders for Projects and Questions

--- Practice Questions/lists_practice.py
+++ Practice Questions/lists_practice.py
@@ -237,3 +237,10 @@ for i in l1:
 
 print(result)
 
+# Problem 13: Write a list comprehension to print the following matrix
+# [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+
+l1 =[[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+
+result = []
+
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-l1 = [[1,2,3],[4,5,6],[7,8,9]]
-
-result = []
-for i in l1:
-    result.append(max(i))
-
-print(result)
